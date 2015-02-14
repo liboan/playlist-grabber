a25351991bd67a127bef05757c143f107825ffc2
Andrew <liboan1997@gmail.com>
2015-02-14 10:24:45 -0500

    reworked search and display functions to work with central video array. Video duration search non-functional.

--- public/main.js
+++ public/main.js
@@ -28,6 +28,25 @@ function initYoutube() {
 	
 }
 
+function searchSong(query, index) { //index = index of songList on which searchSong is being called. list = songList
+	var request = gapi.client.youtube.search.list({
+		type: "video",
+		q: query,
+		part: "snippet",
+		maxResults: 5
+	});
+
+	request.execute(function (response) {
+		var videos = response.result.items;
+		console.log(videos[0].id.videoId + " " + videos[0].snippet.title);
+	});
+}
+
+function pushYTInfo(videos, index) { //pushes to songList
+	songList[index].title = videos[0].snippet.title;
+	songList[index].duration = 0;///FUCK WHY DO I HAVE TO MAKE ANOTHER API CALL
+	songList[index].id = videos[0].id.videoId;
+}
 
 //////SOUNDCLOUD API STUFF//////
 
@@ -44,7 +63,26 @@ function loadPlaylist(url) {
 		console.log(data);
 		if (data.kind === "playlist") {
 			alert("playlist");
-			showPlaylist(data);
+
+			for (var i = 0; i < data.tracks.length; i++) {
+				songList.push({
+					sc: { //SoundCloud info
+						title: data.tracks[i].title,
+						duration: Math.floor(data.tracks[i].duration/1000), // ms ==> s
+						link: data.tracks[i].permalink_url
+					},
+					yt: { //YouTube info- left blank until search returns data
+						title: "",
+						duration: "",
+						id: ""
+					}
+
+				});
+			}
+
+
+			showPlaylist(songList);
+			playlistSearch(songList);
 		}
 		else {
 			alert("Please enter a playlist");
@@ -54,19 +92,58 @@ function loadPlaylist(url) {
 
 ////////////////////////////////
 
-function showPlaylist(data) { //takes SoundCloud API's data object
-	var title = data.title;
-	var tracks = data.tracks;
-	console.log("Playlist Title: " + title);
+var songList = []; //list of songs. Each song object contains song info from SC and YT (title, time, url/id)
 
-	for (var i = 0; i < tracks.length; i++) {
-		console.log(tracks[i].title + " " + msToTime(tracks[i].duration));
+function showPlaylist(list) { //takes songList
+
+	for (var i = 0; i < list.length; i++) {
+		console.log(list[i].sc.title + "\t" + list[i].sc.duration);
+		console.log(list[i].yt.title + "\t" + list[i].yt.duration);
+		console.log("------------------------------------------------------------");
 	}
 }
+
+function playlistSearch(list) { //takes songList
+	for (var i = 0; i < list.length; i++) {
+		searchSong(list[i].sc.title, i);
+	}
+}
+
+
+//////ANNOYING TIME STUFF//////
 
 function msToTime(ms) {
 	var seconds = Math.floor(ms/1000);
 	var minutes = Math.floor(seconds/60);
 	seconds = seconds % 60;
 	return minutes + ":" + seconds;
+}
+
+function hmsToSeconds(timeArray) { //returns seconds when an array from ISOtoTime() is passed in
+	return 3600 * timeArray[0] + 60 * timeArray[1] + timeArray[2]; 
+}
+
+function ISOtoTime(duration) { //YouTube API returns duration in ISO 8601 format PT#H#M#S
+	duration = duration.substring(2); //chop off PT
+	// var h = duration.indexOf("H"); //look for hours, minutes, seconds
+	// var m = duration.indexOf("M");
+	// var s = duration.indexOf("S");
+
+	var stringCheck = ["H","M","S"];
+
+	var time = [];
+
+	for (var i = 0; i < stringCheck.length; i++) {
+		var letter = duration.indexOf(stringCheck[i]);
+		if (letter != -1) {
+			time.push(parseInt(duration.substring(0,letter))); //parse out the number before the letter (H,M,S)
+			duration = duration.substring(letter+1);
+			console.log(i + " " + duration);
+		}
+		else {
+			time.push(0);
+		}
+	}
+	return time;
+
 }
